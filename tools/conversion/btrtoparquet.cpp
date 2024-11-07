807a75eea2043aad873f7cca45b46cac9e032bca
pascalginter <ginpa2000@gmail.com>
2024-11-07 11:21:29 +0100

    Made tools more production ready

--- tools/conversion/btrtoparquet.cpp
+++ tools/conversion/btrtoparquet.cpp
@@ -1,24 +1,45 @@
-#include <arrow/io/api.h>
-#include <parquet/arrow/writer.h>
-
+// ------------------------------------------------------------------------------
 #include <iostream>
-
+// ------------------------------------------------------------------------------
+#include <arrow/io/api.h>
+#include <gflags/gflags.h>
+#include <parquet/arrow/writer.h>
+// ------------------------------------------------------------------------------
 #include "arrow/DirectoryReader.hpp"
 #include "scheme/SchemePool.hpp"
-
-int main(){
+// ------------------------------------------------------------------------------
+DEFINE_string(btr, "btr", "Directory for btr input");
+DEFINE_string(parquet, "parquet", "Parquet output file");
+DEFINE_string(writer_properties, "writer_properties", "Parquet writer properties");
+// ------------------------------------------------------------------------------
+std::shared_ptr<parquet::WriterProperties> getWriterProperties() {
+  std::shared_ptr<parquet::WriterProperties> props =
+   parquet::WriterProperties::Builder()
+     .compression(parquet::Compression::SNAPPY)
+     //->disable_dictionary()->encoding(parquet::Encoding::PLAIN)
+     ->build();
+  return props;
+}
+// ------------------------------------------------------------------------------
+int main(int argc, char** argv){
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   btrblocks::SchemePool::refresh();
-  btrblocks::arrow::DirectoryReader directoryReader("./binary");
+  btrblocks::arrow::DirectoryReader directoryReader(FLAGS_btr);
   std::shared_ptr<arrow::Table> table;
-  if (!directoryReader.ReadTable(&table).ok()){
-    std::cout << "Error reading btr directory\n";
+  auto status = directoryReader.ReadTable(&table);
+  if (!status.ok()) {
+    std::cout << status << "\n";
     exit(1);
   }
-
-  std::cout << table->ToString() << std::endl;
+  std::cout << "Read btr data successfully" << "\n";
 
   std::shared_ptr<arrow::io::FileOutputStream> outfile =
-    arrow::io::FileOutputStream::Open("btr.parquet").ValueOrDie();
-  parquet::arrow::WriteTable(*table.get(), arrow::default_memory_pool(), outfile).ok();
-  std::cout << "done" << std::endl;
+    arrow::io::FileOutputStream::Open(FLAGS_parquet).ValueOrDie();
+  status = parquet::arrow::WriteTable(*table, arrow::default_memory_pool(), outfile, 65536, getWriterProperties());
+  if (!status.ok()) {
+    std::cout << status << "\n";
+    exit(1);
+  }
+  std::cout << "Wrote parquet data successfully\n";
+  return 0;
 }
--- tools/conversion/parquettobtr.cpp
+++ tools/conversion/parquettobtr.cpp
@@ -1,5 +1,8 @@
 #include <arrow/api.h>
 #include <arrow/io/api.h>
+
+#include "gflags/gflags.h"
+
 #include <parquet/arrow/reader.h>
 
 #include <iostream>
@@ -10,27 +13,35 @@
 
 #include "btrfiles.hpp"
 
-int main(){
+DEFINE_string(parquet, "parquet", "Parquet input file");
+DEFINE_string(btr, "btr", "Directory for btr output");
+
+int main(int argc, char** argv){
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   std::shared_ptr<arrow::io::RandomAccessFile> input =
-      arrow::io::ReadableFile::Open("lineitem.parquet").ValueOrDie();
+      arrow::io::ReadableFile::Open(FLAGS_parquet).ValueOrDie();
 
   // Open Parquet file reader
   std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
   auto status = parquet::arrow::OpenFile(input, arrow::default_memory_pool(), &arrow_reader);
    if (!status.ok()) {
-     std::cout << status << std::endl;
+     std::cerr << status << '\n';
+     exit(1);
    }
 
   std::shared_ptr<arrow::Table> table;
   status = arrow_reader->ReadTable(&table);
   if (!status.ok()) {
-    std::cout << status << std::endl;
+    std::cerr << status << '\n';
+    exit(1);
   }
-  std::cout << table->ToString() << "\n";
+  std::cout << "Read parquet data successfully" << "\n";
 
   btrblocks::SchemePool::refresh();
 
   btrblocks::Relation relation= btrblocks::arrow::parseArrowTable(table);
-  btrblocks::files::writeDirectory(relation, "binary/", "stats.txt", "compressionout.txt");
-  std::cout << "done" << std::endl;
+  btrblocks::files::writeDirectory(relation, FLAGS_btr + "/", "stats.txt", "compressionout.txt");
+  std::cout << "Wrote btr data successfully\n";
+  return 0;
 }
